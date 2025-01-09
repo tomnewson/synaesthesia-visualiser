9cd44e2feb6c5e79ba6cdd662f1c068d983671d9
Tom Newson <newsontom@outlook.com>
2025-01-09 00:16:35 +0000

    distortion depends on velocity

--- godot/scripts/midi_processor.gd
+++ godot/scripts/midi_processor.gd
@@ -2,19 +2,25 @@ extends Node3D
 
 @onready var midi_player = $ArlezMidiPlayer
 @onready var environment: Environment = $WorldEnvironment.environment
+@onready var overlay: MeshInstance3D = $Overlay3D
 
-const MAX_VELOCITY = 1000
+const MAX_VELOCITY = 1000.0
 
+const bg_transition_speed = 0.5
+const dist_transition_speed = 0.8
 var active_notes = []
-var target_bg_color = Color.BLACK
-var bg_transition_speed = 0.5
-var no_notes_played = true
+var target_bg_color: Color
+var no_notes_played: bool
+var target_dist_intensity: float
 
 enum Note {ID, TRACK, PITCH, VELOCITY}
 
 func _ready() -> void:
 	midi_player.play()
 	environment.background_color = Color.BLACK
+	target_bg_color = Color.BLACK
+	target_dist_intensity = 0
+	no_notes_played = true
 	
 func _process(delta):
 	var current_bg_colour = environment.background_color
@@ -23,12 +29,26 @@ func _process(delta):
 	var new_val = lerp(current_bg_colour.s, target_bg_color.v, bg_transition_speed * delta)
 	
 	environment.background_color = Color.from_hsv(new_hue, new_sat, new_val)
+	
+	var overlay_material: ShaderMaterial = overlay.mesh.surface_get_material(0)
+	var current_dist_int = overlay_material.get_shader_parameter("distortion_intensity")
+	var new_dist_int = lerp(current_dist_int, target_dist_intensity, dist_transition_speed * delta)
+	overlay_material.set_shader_parameter("distortion_intensity", new_dist_int)
 
 func sigmoid(value: float, steepness: float, midpoint: float) -> float:
 	return 1.0 / (1.0 + exp(-steepness * (value - midpoint)))
+	
+func find_target_bg_color(avg_pitch: float, sum_velocity: float) -> Color:
+	var hue = sigmoid(avg_pitch, 0.2, 50)
+	var sat = remap(sum_velocity, 0, MAX_VELOCITY, 0.2, 0.6)
+	var brightness = remap(sum_velocity, 0, MAX_VELOCITY, 0.4, 0.8)
+	
+	return Color.from_hsv(hue, sat, brightness)
+	
 
-func update_background_color():
+func update_active_notes():
 	if active_notes.is_empty():
+		target_dist_intensity = 0
 		return
 	
 	var sum_pitch = 0
@@ -40,20 +60,16 @@ func update_background_color():
 	var avg_pitch = sum_pitch / len(active_notes)
 	var clamped_sv = clamp(sum_velocity, 0, MAX_VELOCITY)
 	
-	var hue = sigmoid(avg_pitch, 0.2, 50)
-	var sat = remap(clamped_sv, 0, MAX_VELOCITY, 0.2, 0.6)
-	var brightness = remap(clamped_sv, 0, MAX_VELOCITY, 0.4, 0.8)
-	
-	var new_color = Color.from_hsv(hue, sat, brightness)
-	
-	target_bg_color = new_color
+	target_bg_color = find_target_bg_color(avg_pitch, clamped_sv)
 	if no_notes_played:
-		environment.background_color = new_color
+		environment.background_color = target_bg_color
 		no_notes_played = false
+		
+	target_dist_intensity = clamped_sv / MAX_VELOCITY
 
 func _on_midi_receiver_note_on(note_id, note, velocity, track) -> void:
 	active_notes.append([note_id, track, note, velocity])
-	update_background_color()
+	update_active_notes()
 	
 
 func _on_midi_receiver_note_off(note_id, track) -> void:
@@ -61,5 +77,4 @@ func _on_midi_receiver_note_off(note_id, track) -> void:
 		if n[Note.ID] == note_id:
 			active_notes.erase(n)
 			break
-	update_background_color()
-	
+	update_active_notes()
--- godot/scripts/synaesthesia.gd
+++ godot/scripts/synaesthesia.gd
@@ -2,42 +2,57 @@ extends Node3D
 
 var track_paths = [] # int to path scene
 @onready var path_scene = preload("res://scenes/blob.tscn")
-@onready var environment: Environment = $"../../WorldEnvironment".environment
 @export var num_tracks: int = 9
 
-
+const MIN_NOTE = 21
+const MAX_NOTE = 108
+const MIN_VELOCITY = 0
+const MAX_VELOCITY = 127
 
 # Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#for i in range (num_tracks + 1):
-		#var x = remap(i, 0, num_tracks, -7, 7)
-		#var track_instance = path_scene.instantiate()
-		#track_paths.append(track_instance)
-		#track_instance.translate(Vector3(x, 0, 0))
-		#add_child(track_instance)
+func _ready() -> void:
+	for i in range (num_tracks + 1):
+		var x = remap(i, 0, num_tracks, -7, 7)
+		var track_instance = path_scene.instantiate()
+		track_paths.append(track_instance)
+		track_instance.translate(Vector3(x, 0, 0))
+		add_child(track_instance)
+
+# Function to apply sigmoid remap with variable input and output ranges
+func sigmoid_remap(value: float, 
+	input_min: float, input_max: float, 
+	output_min: float, output_max: float, 
+	k: float = 2) -> float:
+	# Normalize input to [0,1]
+	var normalized_value = (value - input_min) / (input_max - input_min)
+
+	# Center normalized_value around 0.5 so that:
+	#   normalized_value = 0.5 --> sigmoid(0) = 0.5
+	# This makes the midpoint of the input range map to the midpoint of the output range.
+	var centered = normalized_value - 0.5
+
+	# Apply the sigmoid function:
+	# You can adjust the scale factor (like 10.0) if you want to "stretch" the middle region.
+	# Removing the extra scale factor is also an option; then just use `exp(-k * centered)`.
+	var sigmoid_value = 1.0 / (1.0 + exp(-k * centered * 10.0))
+
+	# Map the sigmoid result to the output range
+	return output_min + (sigmoid_value * (output_max - output_min))
 
 
-#func _on_midi_receiver_note_on(note_id, note, velocity, track) -> void:
-	## high pitch - small, high up, stringy, opaque, bright, smooth
-	## low pitch - big, low down, wide, transparent, dark, rough
-	## loud - near
-	## quiet - far
-	##var y = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, -3, 5, 4)
-	##var z = sigmoid_remap(velocity, MIN_VELOCITY, MAX_VELOCITY, -10, -7)
-	##var hue = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, 0, 1, 1)
-	##var brightness = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, 0.5, 1, 0.5)
-	##var opacity = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, 0.5, 1)
-	##var roughness = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, 1, 0)
-	#
-	##track_paths[track].add_note(note_id, Vector3(0,y,z), hue, brightness, opacity, roughness)
-	#active_notes.append([note_id, track, note, velocity])
-	#update_background_color()
-	#
-#
-#func _on_midi_receiver_note_off(note_id, track) -> void:
-	##track_paths[track].remove_note(note_id)
-	#for n in active_notes:
-		#if n[Note.ID] == note_id:
-			#active_notes.erase(n)
-	#update_background_color()
-	#
+func _on_midi_receiver_note_on(note_id, note, velocity, track) -> void:
+	# high pitch - small, high up, stringy, opaque, bright, smooth
+	# low pitch - big, low down, wide, transparent, dark, rough
+	# loud - near
+	# quiet - far
+	var y = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, -3, 5)
+	var z = sigmoid_remap(velocity, MIN_VELOCITY, MAX_VELOCITY, -10, -7)
+	var hue = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, 0, 1, 1)
+	var brightness = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, 0.5, 1, 0.5)
+	var opacity = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, 0.5, 1)
+	var roughness = sigmoid_remap(note, MIN_NOTE, MAX_NOTE, 1, 0)
+	
+	track_paths[track].add_note(note_id, Vector3(0,y,z), hue, brightness, opacity, roughness)
+
+func _on_midi_receiver_note_off(note_id, track) -> void:
+	track_paths[track].remove_note(note_id)
